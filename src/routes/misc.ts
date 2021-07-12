874c911ae4de61093f7d762dc4e9392887bc6de8
JensMeire <jens.meire>
2021-07-12 15:24:24 +0200

    [Feat] Add getExternalResource to from nginc to api

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -1,10 +1,14 @@
-import { Router } from 'express';
-import { MessageTypes } from '../types';
+import {Router} from 'express';
+import {MessageTypes} from '../types';
 import Chat from '../models/chat';
-import { uuidv4 } from '../common';
-import { config } from '../config/config';
-import { getChat, persistChat } from '../service/dataService';
-import { getMyLocation } from '../service/locationService';
+import {uuidv4} from '../common';
+import {config} from '../config/config';
+import {getChat, persistChat} from '../service/dataService';
+import {getMyLocation} from '../service/locationService';
+import http from "http";
+import express from "express"
+import {HttpError} from '../types/errors/httpError';
+import {StatusCodes} from 'http-status-codes';
 
 const router = Router();
 
@@ -22,12 +26,22 @@ router.get('/test', async (req, res) => {
     const chat = new Chat(id, [], false, [], 'test', false, config.userid, {});
 
     persistChat(chat);
-    res.json({ success: true });
+    res.json({success: true});
 });
 
 router.get('/yggdrasil_address', async (req, res) => {
     let myLocation = await getMyLocation();
     res.json(myLocation);
 });
+
+router.get('/getexternalresource', async (req: express.Request, res: express.Response) => {
+    const resource = req.query.resource as string | undefined;
+    if (!resource)
+        throw new HttpError(StatusCodes.BAD_REQUEST, "No resource was given");
+    http.get(resource, function (resp) {
+        res.setHeader('content-disposition', "attachment");
+        resp.pipe(res);
+    });
+})
 
 export default router;
