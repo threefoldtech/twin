1dc1053a37b3a3d39b2a5bda94555ca81dd97e8f
EdwardKerckhof <edward@zonedev.be>
2022-02-23 13:43:58 +0100

    Add config and redis connection

--- src/nest/features/app/app.module.ts
+++ src/nest/features/app/app.module.ts
@@ -1,7 +1,35 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from '../../config/config';
 import { LocationModule } from '../location/location.module';
+import Joi from 'joi';
+import LoggerMiddleware from '../../middleware/logger.middleware';
+import { RedisModule } from 'nestjs-redis';
 
 @Module({
-    imports: [LocationModule],
+    imports: [
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                PORT: Joi.number().required(),
+                NODE_ENV: Joi.string().required(),
+                REDIS_HOST: Joi.string().required(),
+                REDIS_PORT: Joi.number().required(),
+                REDIS_DB: Joi.number().required(),
+                REDIS_PASSWORD: Joi.string().required(),
+            }),
+            load: [config],
+            isGlobal: true,
+            cache: true,
+        }),
+        RedisModule.forRootAsync({
+            useFactory: (configService: ConfigService) => configService.get('redis'),
+            inject: [ConfigService],
+        }),
+        LocationModule,
+    ],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
--- src/nest/main.ts
+++ src/nest/main.ts
@@ -1,6 +1,8 @@
-import { INestApplication } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, INestApplication, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './features/app/app.module';
+import { ConfigService } from '@nestjs/config';
+import getLogLevels from './utils/get-log-levels';
 
 /**
  * Bootstrap creates and returns a NestJS application.
@@ -8,5 +10,20 @@ import { AppModule } from './features/app/app.module';
  */
 export default async function bootstrap(): Promise<INestApplication> {
     const app = await NestFactory.create(AppModule);
+
+    // init config service
+    const configService = app.get<ConfigService>(ConfigService);
+
+    app.setGlobalPrefix('api');
+
+    // logger middleware depending on node environment
+    app.useLogger(getLogLevels(configService.get<string>('NODE_ENV') === 'production'));
+
+    // global validation pipe for class-validator
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+    // global class serialization for class-transformer
+    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
     return app;
 }
