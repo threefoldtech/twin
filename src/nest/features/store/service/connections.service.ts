38f5160963d2c1f2eddbd1df8cd8e94a1d2df89d
EdwardKerckhof <edward@zonedev.be>
2022-02-28 15:44:04 +0100

    Implement add and get connections

--- src/nest/features/store/service/connections.service.ts
+++ src/nest/features/store/service/connections.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Repository } from 'redis-om';
 
 import { DbService } from '../../db/service/db.service';
@@ -9,11 +9,14 @@ export class ConnectionService {
     private connectionRepo: Repository<Connection>;
 
     constructor(private readonly _dbService: DbService) {
-        this.connectionRepo = _dbService.createRepository(connectionSchema);
-        this._dbService.createIndex(this.connectionRepo);
+        this.connectionRepo = this._dbService.createRepository(connectionSchema);
+        // this._dbService.createIndex(this.connectionRepo);
     }
 
     /**
+     * Adds a twin connection to Redis.
+     * @param {string} connection - IPv6 to add.
+     * @return {string} - Entity ID.
      */
     async addConnection(connection: string): Promise<string> {
         try {
@@ -21,6 +24,19 @@ export class ConnectionService {
             return await this.connectionRepo.save(connectionEntity);
         } catch (error) {
             throw new BadRequestException(error);
+        }
+    }
+
+    /**
+     * Gets all connections stored in Redis.
+     * @param {number} pageSize - Amount of items to return, defaults to 20.
+     * @return {Connection[]} - The connection list.
+     */
+    async getConnections(pageSize = 20): Promise<Connection[]> {
+        try {
+            return await this.connectionRepo.search().returnAll({ pageSize });
+        } catch (error) {
+            throw new InternalServerErrorException(error);
         }
     }
 }
--- src/nest/features/store/service/keys.service.ts
+++ src/nest/features/store/service/keys.service.ts
@@ -15,14 +15,15 @@ export class KeyService {
         private readonly _dbService: DbService,
         private readonly _encryptionService: EncryptionService
     ) {
-        this.keyRepo = _dbService.createRepository(keySchema);
-        this._dbService.createIndex(this.keyRepo);
+        this.keyRepo = this._dbService.createRepository(keySchema);
+        // this._dbService.createIndex(this.keyRepo);
     }
 
     /**
      * Updates either private or public key based on the key type.
      * @param {Uint8Array} pk - Private/Public key in Uint8Array format.
      * @param {KeyType} keyType - Identifies a key as public or private.
+     * @return {string} - Entity ID.
      */
     async updateKey({ pk, keyType }: { pk: Uint8Array; keyType: KeyType }): Promise<string> {
         const pkString = this._encryptionService.uint8ToBase64(pk);
