43ca2f5b178c1881ee227b422c93d6731f8b2b09
EdwardKerckhof <edward@zonedev.be>
2022-02-28 09:23:10 +0100

    Fix location dependency issue

--- src/nest/features/location/location.module.ts
+++ src/nest/features/location/location.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { LocationService } from './service/location.service';
 import { LocationController } from './controller/location.controller';
+import { EncryptionModule } from '../encryption/encryption.module';
 
 @Module({
+    imports: [EncryptionModule],
     providers: [LocationService],
     controllers: [LocationController],
 })
