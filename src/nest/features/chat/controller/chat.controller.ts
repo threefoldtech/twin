10c27fba8c6943059368f7ea7d9553361c0746cf
EdwardKerckhof <edward@zonedev.be>
2022-03-02 17:03:59 +0100

    Continue chat controller

--- src/nest/features/chat/controller/chat.controller.ts
+++ src/nest/features/chat/controller/chat.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 
+import { AuthGuard } from '../../../guards/auth.guard';
 import { CreateChatDTO } from '../dtos/chat.dto';
 import { Chat } from '../models/chat.model';
 import { ChatService } from '../service/chat.service';
@@ -9,26 +10,34 @@ export class ChatController {
     constructor(private readonly _chatService: ChatService) {}
 
     @Post()
+    @UseGuards(AuthGuard)
     async createChat(@Body() createChatDTO: CreateChatDTO) {
-        // TODO: get data from body request
         // IMPORTANT: Incoming body data will need to be parsed like this or it will not be able
         // to be stored in Redis.
+        // EXAMPLE
+        // name: 'test',
+        // contacts: ['{"id": "1", "location": "localhost"}', '{"id": "2", "location": "localhost"}'],
+        // messages: [
+        //     '{"chatId": "testchat", "from": "edward", "to": "jens", "body": "Test message", "timestamp": "2022-03-02", "type": "MESSAGE", "subject": "Subject", "signatures": ["edward"], "replies": []}',
+        // ],
+        // acceptedChat: true,
+        // adminId: 'edward',
+        // read: ['edward'],
+        // isGroup: false,
+        // draft: [],
         const createdChat = await this._chatService.createChat({
-            name: 'test',
-            contacts: ['{"id": "1", "location": "localhost"}', '{"id": "2", "location": "localhost"}'],
-            messages: [
-                '{"chatId": "testchat", "from": "edward", "to": "jens", "body": "Test message", "timestamp": "2022-03-02", "type": "MESSAGE", "subject": "Subject", "signatures": ["edward"], "replies": [{"chatId": "testchat", "from": "jens", "to": "edward", "body": "Test reply message", "timestamp": "2022-03-02", "type": "MESSAGE", "subject": "Reply", "signatures": ["jens"], "replies": []}]}',
-                '{"chatId": "testchat", "from": "jens", "to": "edward", "body": "Test reply message", "timestamp": "2022-03-02", "type": "MESSAGE", "subject": "Reply", "signatures": ["jens"], "replies": []}',
-            ],
-            acceptedChat: true,
-            adminId: 'edward',
-            read: ['edward'],
-            isGroup: false,
-            draft: [
-                '{"chatId": "testchat", "from": "edward", "to": "jens", "body": "Test draft message", "timestamp": "2022-03-02", "type": "MESSAGE", "subject": "Draft", "signatures": ["edward"], "replies": []}',
-            ],
+            name: createChatDTO.name,
+            contacts: createChatDTO.contacts,
+            messages: createChatDTO.messages,
+            acceptedChat: createChatDTO.acceptedChat,
+            adminId: createChatDTO.adminId,
+            read: createChatDTO.read,
+            isGroup: createChatDTO.isGroup,
+            draft: createChatDTO.draft,
         });
 
+        // TODO: send new chat event to socket.io
+        // TODO: create socket.io service and connection with NestJS app
         return {
             ...createdChat.entityData,
             messages: createdChat.parseMessages(),
