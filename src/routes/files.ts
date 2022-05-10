7b4563a76d4e49fb0636f363711c4b743884807f
JorisBassle <joris.bassle@gmail.com>
2022-05-10 12:07:54 +0200

    changed imports from pulling development

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -1,12 +1,12 @@
 import express, { Router } from 'express';
 import { UploadedFile } from 'express-fileupload';
-import express from 'express';
 
 import { uuidv4 } from '../common';
 import { config } from '../config/config';
 import Message from '../models/message';
 import { sendMessageToApi } from '../service/apiService';
 import { getChat, persistChat, saveFile } from '../service/dataService';
+import { hasSpecialCharacters } from '../service/fileService';
 import { appendSignatureToMessage } from '../service/keyService';
 import { getMyLocation } from '../service/locationService';
 import { parseMessage } from '../service/messageService';
@@ -14,7 +14,6 @@ import { sendEventToConnectedSockets } from '../service/socketService';
 import { getFullIPv6ApiLocation } from '../service/urlService';
 import { FileMessageType, MessageTypes } from '../types';
 import { Path, saveTempFile } from '../utils/files';
-import { hasSpecialCharacters } from '../service/fileService';
 
 const router = Router();
 
