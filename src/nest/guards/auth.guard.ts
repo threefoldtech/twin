8f30c3d35d16bfbd747b84d9f63fec005597db05
EdwardKerckhof <edward@zonedev.be>
2022-05-19 17:02:05 +0200

    Re-enable auth guard

--- src/nest/guards/auth.guard.ts
+++ src/nest/guards/auth.guard.ts
@@ -11,14 +11,13 @@ export class AuthGuard implements CanActivate {
     ) {}
 
     async canActivate(context: ExecutionContext) {
-        // const req = context.switchToHttp().getRequest();
+        const req = context.switchToHttp().getRequest();
 
-        // const userId = req.session?.userId;
-        // const isDevMode = this._configService.get<string>('node_env') === 'development';
-        // const yggdrasilInitialised = this._yggdrasilService.isInitialised();
-        // if (!userId && (!isDevMode || !yggdrasilInitialised)) return false;
+        const userId = req.session?.userId;
+        const isDevMode = this._configService.get<string>('node_env') === 'development';
+        const yggdrasilInitialised = this._yggdrasilService.isInitialised();
+        if (!userId && (!isDevMode || !yggdrasilInitialised)) return false;
 
-        // return userId === this._configService.get<string>('userId');
-        return true;
+        return userId === this._configService.get<string>('userId');
     }
 }
