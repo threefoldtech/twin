5e87b3e2302bcc218962feb663906b9b38909c59
JorisBassle <joris.bassle@gmail.com>
2022-05-24 09:30:51 +0200

    #448 remove commented code

--- src/routes/blocked.ts
+++ src/routes/blocked.ts
@@ -5,10 +5,6 @@ import { getBlocklist, persistBlocklist } from '../service/dataService';
 
 const router = Router();
 
-// router.get('/', requiresAuthentication, (_req: express.Request, res: express.Response) => {
-//     res.json(getBlocklist());
-// });
-
 router.delete('/:name', requiresAuthentication, (req: express.Request, res: express.Response) => {
     persistBlocklist(getBlocklist().filter(b => b != req.params.name));
     res.json({ status: 'success' });
